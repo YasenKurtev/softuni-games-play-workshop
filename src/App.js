d42e3236b67b5051adc4a6b2d19dc1c39e8c346e
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-08-15 13:13:36 +0300

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
-
+    return (
+        <div id="box">
+            <Navigation />
+        </div>
+    )
 }
 
 export default App;
